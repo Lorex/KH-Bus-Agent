a6926da8de5b8228ebcce8c25bb75e6efa340ccc
Lorex <joe50613@yahoo.com.tw>
2014-06-12 10:21:58 +0800

    進度：取得公車路線列表

--- KH Bus Agent/Form1.cs
+++ KH Bus Agent/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace KH_Bus_Agent
 {
@@ -16,5 +17,35 @@ namespace KH_Bus_Agent
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RefRoute();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefRoute();
+        }
+        private void RefRoute()
+        {
+            // Load XML Document
+            XmlDocument doc = new XmlDocument();
+            doc.Load("http://122.146.229.210/xmlbus2/StaticData/GetRoute.xml");
+
+            //Select Node List
+            XmlNodeList NodeLists = doc.SelectNodes("BusDynInfo/BusInfo/Route");
+
+            //Read Node
+            foreach (XmlNode Single in NodeLists)
+            {
+                comboBox1.Items.Add(Single.Attributes["ID"].Value);
+            }
+        }
+        private void RefEstimate()
+        {
+
+        }
     }
+
 }
